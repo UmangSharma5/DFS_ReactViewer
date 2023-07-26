579cc7df8b65ec0b59f5a3f592c6b38b7a227938
RhythmAgg <aggarwalrhythm2211@gmail.com>
2023-07-26 16:37:05 +0530

    fix; base-url in config

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 const PORT = 5000;
 const config = {
     BASE_URL : "http://localhost:"+String(PORT)+"/hv"
-    // BASE_URL = "https://datafoundation.iiit.ac.in/hv"
+    // BASE_URL : "https://datafoundation.iiit.ac.in/hv"
 }
 
 export {
