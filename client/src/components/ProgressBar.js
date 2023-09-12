1b25cfd796447dd5120ea26fedca8ed4198a4f02
Harsh Bansal <102587800+HarsH-BansaL16@users.noreply.github.com>
2023-09-12 19:20:25 +0530

    Created New Component with Final Changes

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -5,8 +5,8 @@ function ProgressBar(props) {
 
   return (
     <div className='progress-container'>
-        <progress id="uploadProgressBar" value="0" max="100" style={{ display: (props.display ? "inline" : "none") }}></progress>
-        <label id='uploadProgressBarLabel' style={{ display: (props.display ? "inline" : "none") }}>{props.progressValue === 100 ? 'Upload Completed' : `${props.progressValue}%`}</label>
+        <progress className="uploadProgressBar" value={props.progressValue} max="100" style={{ display: (props.display ? "inline" : "none")}}></progress>
+        <label className='uploadProgressBarLabel' style={{ display: (props.display ? "inline" : "none") }}>{props.progressValue === 100 ? 'Upload Completed!' : `${props.progressValue}%`}</label>
     </div>
   )
 }
