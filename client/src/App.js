6a3eb078e53a55e02e8103ed6a17b0860c4b75de
Harsh Bansal <102587800+HarsH-BansaL16@users.noreply.github.com>
2023-09-12 23:09:07 +0530

    Feat/#6/create upload progress bar (#7)
    Feat/#6/create upload progress bar
    
    
    * Update App.js
    
    * Update server.js
    
    * Update config.js
    
    * Update App.css
    
    * Styling for Progress Bar Modified
    
    * Changed Color of Progress Bar to Blue
    
    * Update .gitignore
    
    * Update App.js
    
    * Update deleteBucket.js
    
    * Worked on Refactoring
    
    * Created New Component with Final Changes
    
    * Update config.js
    
    * Update server.js
    
    * Update config.js
    
    * Update server.js
    
    * Update .gitignore
    
    * Update App.css
    
    * Changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 import GetFiles from './components/GetFiles';
+import ProgressBar from './components/ProgressBar';
 import { config } from './config'; 
 
 
@@ -11,6 +12,8 @@ function App(props) {
     name: ""
   });
   const [isUploaded,setIsUploaded] = useState(false);
+  const [displayProgressBar, setDisplayProgressBar] = useState(false)
+  const [progressValue,setProgressValue] = useState(0)
   const [fileInfo,setFileInfo] = useState({});
 
   const email = JSON.parse(localStorage.getItem("dfs-user")).user.user_email.toLowerCase();
@@ -37,26 +40,38 @@ function App(props) {
   async function uploadFile(e) {
     e.preventDefault();
     const formData = new FormData();
+    setDisplayProgressBar(true)
     formData.append('file', currentFile.name);
     let bucketURL = config.BASE_URL+"/objects/" + shortEmail;
     try {
-      console.log("Initiating upload")
-      const response = await axios.post(bucketURL, formData,
-      {
-          headers: {
-              'authorization': 'Bearer ' + JSON.parse(localStorage.getItem('dfs-user'))?.['token'],
-          }
-      });
-      console.log("Upload complete");
-      console.log(response.data.filename);
-      setIsUploaded(true);
-      console.log("resp-",response);
+      console.log('Initiating upload')
+      const response = await axios.post(bucketURL, formData, {
+        headers: {
+          authorization:
+            'Bearer ' + JSON.parse(localStorage.getItem('dfs-user'))?.['token'],
+        },
+        // Added On Upload Progress Config to Axios Post Request
+        onUploadProgress: function (progressEvent) {
+          const percentCompleted = Math.round((progressEvent.loaded / progressEvent.total) * 100)
+          setProgressValue(percentCompleted)
+        },
+      })
+
+      setTimeout(function () {
+        setProgressValue(0)
+        setDisplayProgressBar(false)
+      }, 3000)
+
+      console.log('Upload Complete')
+      console.log(response.data.filename)
+      setIsUploaded(true)
+      console.log('resp-', response)
       setCurrentFile((prevValue) => ({
         ...prevValue,
-        name:response.data.filename,
+        name: response.data.filename,
         format: response.data.format,
-        count: prevValue.count+1,
-      }))     
+        count: prevValue.count + 1,
+      }))
     } catch (error) {
       console.log(error);
     }
@@ -71,6 +86,7 @@ function App(props) {
             <input type="file" id="fileInput" onChange={handleChange} className="input-file"/>
             <button type="submit" onClick={uploadFile} className="upload-button">Upload</button>
           </form>
+          {displayProgressBar? <ProgressBar progressValue={progressValue}/>:<></>}
         </div>
         <button id="logout-btn" onClick={handleClick}>Logout</button>
       </div>
