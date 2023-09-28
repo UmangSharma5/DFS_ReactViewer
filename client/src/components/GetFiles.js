899f9a82be0ff326df24a47c5668e78447eb9ca5
Harsh Bansal <102587800+HarsH-BansaL16@users.noreply.github.com>
2023-09-28 14:57:21 +0530

    Instant Image Delete

--- client/src/components/GetFiles.js
+++ client/src/components/GetFiles.js
@@ -57,7 +57,12 @@ function GetFiles(props){
                 }
             })
             .then(response => {
-                const updatedData = backendData.filter(currFile => currFile !== delFileName);
+                // const updatedData = backendData.filter(currFile => currFile !== delFileName);
+                const updatedData = Object.values(backendData).filter(
+                  (currFile) =>
+                    currFile.name !== file.name ||
+                    currFile.format !== file.format
+                )
                 setBackendData(updatedData);
                 setCurrFileName(null);
             })
