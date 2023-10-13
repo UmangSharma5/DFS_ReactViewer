942c5bd96c3126a92e2410b744fa495798318930
RhythmAgg <aggarwalrhythm2211@gmail.com>
2023-10-13 18:07:31 +0530

    Socket-backend-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,123 +1,161 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import GetFiles from './components/GetFiles';
-import ProgressBar from './components/ProgressBar';
-import { config } from './config'; 
-import { toast } from 'react-toastify'
-
-
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import GetFiles from "./components/GetFiles";
+import ProgressBar from "./components/ProgressBar";
+import { config } from "./config";
+import { toast } from "react-toastify";
+import { io } from "socket.io-client";
 
 function App(props) {
-  const [currentFile,setCurrentFile] =useState({
+  const [currentFile, setCurrentFile] = useState({
     count: 0,
-    name: ""
+    name: "",
   });
-  const [isUploaded, setIsUploaded] = useState(false)
-  const [displayProgressBar, setDisplayProgressBar] = useState(false)
-  const [progressValue, setProgressValue] = useState(0)
-  const currentFileSelected = useRef(null)
-  const [fileInfo, setFileInfo] = useState({})
+  const [isUploaded, setIsUploaded] = useState(false);
+  const [displayProgressBar, setDisplayProgressBar] = useState(false);
+  const [progressValue, setProgressValue] = useState(0);
+  const currentFileSelected = useRef(null);
+  const [fileInfo, setFileInfo] = useState({});
 
-  const email = JSON.parse(localStorage.getItem("dfs-user")).user.user_email.toLowerCase();
-  let shortEmail ='';
+  const email = JSON.parse(
+    localStorage.getItem("dfs-user")
+  ).user.user_email.toLowerCase();
+  let shortEmail = "";
   for (let i = 0; i < email.length; i++) {
-    const charCode = email.charCodeAt(i);   
-    if ((charCode >= 48 && charCode <= 57) || (charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+    const charCode = email.charCodeAt(i);
+    if (
+      (charCode >= 48 && charCode <= 57) ||
+      (charCode >= 65 && charCode <= 90) ||
+      (charCode >= 97 && charCode <= 122)
+    ) {
       shortEmail += email.charAt(i);
     }
   }
-  
-  function handleClick(){
+
+  function handleClick() {
     props.logout();
   }
 
-  function handleChange(e){
-    const file =  e.target.files[0];
-    setCurrentFile((prevValue)=>({
+  function handleChange(e) {
+    const file = e.target.files[0];
+    setCurrentFile((prevValue) => ({
       ...prevValue,
-      name : file
-    }))
-  };
-  
+      name: file,
+    }));
+  }
+
   async function uploadFile(e) {
     e.preventDefault();
+    const socket = io.connect('http://localhost:5000');
+    socket.on('connect', () => {
+      console.log('Connected:', socket.connected); // Should be true
+    });
+    socket.on('progress',(progress_data) => {
+      console.log(progress_data)
+    })
+    socket.on('error', (error) => {
+      console.error('Socket.IO error:', error);
+    });
     const formData = new FormData();
-    setDisplayProgressBar(true)
-    formData.append('file',currentFile.name );
-    let bucketURL = config.BASE_URL+"/objects/" + shortEmail;
-    
+    setDisplayProgressBar(true);
+    formData.append("file", currentFile.name);
+    let bucketURL = config.BASE_URL + "/objects/" + shortEmail;
 
-      let res = await axios.get(config.BASE_URL+"/isUploaded/"+shortEmail,{
-        headers: {
-          authorization:'Bearer ' +JSON.parse(localStorage.getItem('dfs-user'))?.['token'],
-        },
-        params:{
-          fileName:currentFile.name.name,
+    let res = await axios.get(config.BASE_URL + "/isUploaded/" + shortEmail, {
+      headers: {
+        authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("dfs-user"))?.["token"],
+      },
+      params: {
+        fileName: currentFile.name.name,
+      },
+    });
+
+    if (res != undefined && res.data.isUploaded == 1) {
+      toast.warn("Image Already Exists");
+      setDisplayProgressBar(false);
+    } else {
+      try {
+        console.log("Initiating upload");
+        let response = await axios.post(bucketURL, formData, {
+          headers: {
+            authorization:
+              "Bearer " +
+              JSON.parse(localStorage.getItem("dfs-user"))?.["token"],
+          },
+          // Added On Upload Progress Config to Axios Post Request
+          onUploadProgress: function (progressEvent) {
+            const percentCompleted = Math.round(
+              (progressEvent.loaded / progressEvent.total) * 100
+            );
+            setProgressValue(percentCompleted);
+          },
+        });
+        if (response.status === 200) {
+          toast.info("Upload is in Progress....Please check after some time");
+        } else {
+          toast.error("Error in Uploading File");
         }
-      })
-
-      if(res != undefined && res.data.isUploaded == 1){
-        toast.warn("Image Already Exists");
-        setDisplayProgressBar(false);
+        setTimeout(function () {
+          setProgressValue(0);
+          setDisplayProgressBar(false);
+        }, 3000);
+        console.log("Upload complete");
+        currentFileSelected.current.value = null;
+        setIsUploaded(true);
+        setCurrentFile((prevValue) => ({
+          ...prevValue,
+          name: response.data.filename,
+          format: response.data.format,
+          count: prevValue.count + 1,
+        }));
+      } catch (error) {
+        console.log(error);
       }
-      else{
-        try{
-          console.log("Initiating upload")
-          let response = await axios.post(bucketURL, formData,{
-              headers: {
-                 authorization:
-                'Bearer ' + JSON.parse(localStorage.getItem('dfs-user'))?.['token'],
-            },
-            // Added On Upload Progress Config to Axios Post Request
-            onUploadProgress: function (progressEvent) {
-              const percentCompleted = Math.round((progressEvent.loaded / progressEvent.total) * 100)
-              setProgressValue(percentCompleted)
-            },
-          })
-          if (response.status === 200) {
-            toast.info('Upload is in Progress....Please check after some time')
-          } else {
-            toast.error('Error in Uploading File')
-          }
-          setTimeout(function () {
-            setProgressValue(0)
-            setDisplayProgressBar(false)
-          }, 3000)
-          console.log("Upload complete");
-          currentFileSelected.current.value = null
-          setIsUploaded(true);
-          setCurrentFile((prevValue) => ({
-            ...prevValue,
-            name: response.data.filename,
-            format: response.data.format,
-            count: prevValue.count + 1,
-          }))
-        }catch (error) {
-          console.log(error);
-        }
-      }  
-  }
+    }
     
+  }
+
   return (
     <div className="App">
-      <div className='main-btn'>
+      <div className="main-btn">
         <div className="form-container">
           <form>
-            <input type="file" ref={currentFileSelected} id="fileInput" onChange={handleChange} className="input-file"/>
-            <button type="submit" onClick={uploadFile} className="upload-button">Upload</button>
+            <input
+              type="file"
+              ref={currentFileSelected}
+              id="fileInput"
+              onChange={handleChange}
+              className="input-file"
+            />
+            <button
+              type="submit"
+              onClick={uploadFile}
+              className="upload-button"
+            >
+              Upload
+            </button>
           </form>
-          {displayProgressBar? <ProgressBar progressValue={progressValue}/>:<></>}
+          {displayProgressBar ? (
+            <ProgressBar progressValue={progressValue} />
+          ) : (
+            <></>
+          )}
         </div>
-        <button id="logout-btn" onClick={handleClick}>Logout</button>
+        <button id="logout-btn" onClick={handleClick}>
+          Logout
+        </button>
       </div>
-      <div className='get-files'>
-        <GetFiles fileObj={currentFile} uploadStatus={isUploaded} email={shortEmail} />
+      <div className="get-files">
+        <GetFiles
+          fileObj={currentFile}
+          uploadStatus={isUploaded}
+          email={shortEmail}
+        />
       </div>
     </div>
   );
-
 }
 
 export default App;
-
--- server/server.js
+++ server/server.js
@@ -3,8 +3,13 @@ const app = express();
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import path from 'path';
+import http from 'http'
 import { fileURLToPath } from 'url';
 import require_auth from './middleware/Auth.js';
+import { Server } from 'socket.io'
+const server = http.createServer(app);
+
+
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'build')));
 
-import objectsRoute from './routes/objects.js';
+import {router as objectsRoute,updateSocket} from './routes/objects.js';
 import getURL from './routes/getURL.js';
 import deleteBucket from './routes/deleteBucket.js';
 import deleteObject from './routes/deleteObject.js';
@@ -35,6 +40,26 @@ app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(5000, function () {
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  },
+});
+
+// Add this
+// Listen for when the client connects via socket.io-client
+io.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  updateSocket(socket)
+
+  //when disconnect from client
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    updateSocket(0)
+  });
+});
+
+server.listen(5000, function () {
   console.log('Server started on port 5000')
 })
