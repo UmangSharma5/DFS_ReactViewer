0c55daf72efef4e868fe9c09c03c02219291c380
Harsh Bansal <102587800+HarsH-BansaL16@users.noreply.github.com>
2023-11-07 15:46:40 +0530

    Changed NavBar.js Location

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import './NavBar.css'
+import { useNavigate } from 'react-router-dom'
 
 function NavBar(props) {
+
+  const navigate = useNavigate()
 
   function handleClick() {
     props.logout()
   }
 
+  function handleHome() {
+    navigate('/')
+  }
+
   return (
     <div className='navbar'>
-      <div id='ihub-logo' ></div>
+      <div id='ihub-logo'  onClick={handleHome}></div>
       <p id='heading'>HISTOPATHOLOGY VIEWER</p>
       <button id="logout-btn" onClick={handleClick}>Logout</button>
     </div>
