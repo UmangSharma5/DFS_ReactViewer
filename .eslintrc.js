7381b344d754e2e7acdfc8307d0135e3056b0058
Umang <umangsharma6542@gmail.com>
2023-12-12 16:46:36 +0000

    checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,19 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+    // {
+    //   env: {
+    //     node: true,
+    //   },
+    //   files: ['.eslintrc.{js,cjs}'],
+    //   parserOptions: {
+    //     sourceType: 'script',
+    //   },
+    // },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
